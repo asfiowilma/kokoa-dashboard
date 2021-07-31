96ded79553fc619a7e5f3cf2eb03b7f06256ffa2
asfiowilma <asfiolithaw@gmail.com>
2021-07-31 19:28:59 +0700

    hide scrollbar

--- components/layout.js
+++ components/layout.js
@@ -15,7 +15,7 @@ export default function Layout({ children, labelName }) {
       <div className="flex flex-col items-center w-full min-h-screen h-screen bg-neutral">
         <div className="w-full flex-1 drawer drawer-mobile px-4 md:px-8 pt-4 md:pt-8">
           <input id={labelName} type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content flex flex-col items-center pl-0 md:pl-6 scrollbar-hide">
+          <div className="drawer-content flex flex-col items-center pl-0 md:pl-6 scrollbar scrollbar-hidden">
             <Navbar sidebarLabel={labelName} />
             <main className="flex w-full flex-1 items-start">{children}</main>
           </div>
